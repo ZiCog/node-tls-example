f95b0d935c0d0fe5260f79fccfb5c7fb0a5ec0f7
michael <michael@debian>
2013-04-03 22:57:47 +0300

    Updated inline with tls-client-2.js

--- tls-client-2.js
+++ tls-client-2.js
@@ -3,15 +3,10 @@
 var tls = require('tls'),
     fs = require('fs');
 
-/*
 var options = {
-   key: fs.readFileSync('client-key-2.pem'),
-   cert: fs.readFileSync('client-crt-2.pem')
-};
-*/
-var options = {
-   key: fs.readFileSync('ssl/client-2.key'),
-   cert: fs.readFileSync('ssl/client-2.crt')
+    key: fs.readFileSync('ssl/client-2.key'),
+    cert: fs.readFileSync('ssl/client-2.crt'),
+    rejectUnauthorized: false
 };
 
 var conn = tls.connect(8000, options, function () {
@@ -19,12 +14,18 @@ var conn = tls.connect(8000, options, function () {
         console.log("Connection authorized by a Certificate Authority.");
     } else {
         console.log("Connection not authorized: " + conn.authorizationError);
+	console.log("Eeek:", conn.authorizationError);
     }
     console.log();
+});
+
+conn.on("error", function(err) {
+    console.log("Eeek:", err.toString());
 });
 
 conn.on("data", function (data) {
     console.log(data.toString());
     //conn.end();
 });
+
 
